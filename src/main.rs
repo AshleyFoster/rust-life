4f14cd794bef4495831e3230db2557ebb8f20865
Ashley Foster <ashley@ashleyfoster.me>
2017-10-23 23:49:31 -0400

    Add basic game of life
    This creates a 4x4 grid and applies the game of life rules once to the
    grid.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,102 @@
 fn main() {
-    println!("Hello, world!");
+    let grid = vec![
+        vec!["_", "_", "x", "_"],
+        vec!["x", "x", "x", "_"],
+        vec!["_", "_", "x", "_"],
+        vec!["_", "_", "_", "_"],
+    ];
+
+    // println!("Now {:?} will print!", grid);
+    print_grid(&grid);
+
+    let mut new_grid = grid.clone();
+
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            let neighbors = neighbor_count(&grid, x, y);
+
+            if is_alive(&grid, x, y) {
+                if neighbors >= 4 {
+                    new_grid[y][x] = "_";
+                }
+
+                if neighbors <= 1 {
+                    new_grid[y][x] = "_";
+                }
+
+            } else {
+                if neighbors == 3 {
+                    new_grid[y][x] = "x";
+                }
+            }
+        }
+    }
+
+    print_grid(&new_grid);
+}
+
+fn neighbor_count(grid: &Vec<Vec<&str>>, current_x: usize, current_y: usize) -> usize {
+    let mut count = 0;
+    if current_x > 0 && current_y > 0 {
+        if is_alive(grid, current_x - 1, current_y - 1) {
+            count += 1;
+        }
+    }
+
+    if current_y > 0 {
+        if is_alive(grid, current_x, current_y - 1) {
+            count += 1;
+        }
+
+        if is_alive(grid, current_x + 1, current_y - 1) {
+            count += 1;
+        }
+    }
+
+    if current_x > 0 {
+        if is_alive(grid, current_x - 1, current_y) {
+            count += 1;
+        }
+
+        if is_alive(grid, current_x - 1, current_y + 1) {
+            count += 1;
+        }
+    }
+
+    if is_alive(grid, current_x, current_y + 1) {
+        count += 1;
+    }
+
+    if is_alive(grid, current_x + 1, current_y + 1) {
+        count += 1;
+    }
+
+    if is_alive(grid, current_x + 1, current_y) {
+        count += 1;
+    }
+
+    count
+}
+
+fn is_alive(grid: &Vec<Vec<&str>>, x: usize, y: usize) -> bool {
+    match grid.get(y) {
+        None => false,
+        Some(row) => {
+            match row.get(x) {
+                None => false,
+                Some(cell) => {
+                    *cell == "x"
+                }
+            }
+        }
+    }
+}
+
+fn print_grid(grid: &Vec<Vec<&str>>) -> () {
+    for row in grid {
+        for cell in row.iter() {
+            print!("{}", cell);
+        }
+        println!("");
+    }
 }
